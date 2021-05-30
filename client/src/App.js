e680a417b7f4640d38cc76d59c4a7d82a47eec96
Pavel Krokhmal <pavel.krokhmal.mn@outlook.com>
2021-05-30 12:06:37 +0300

    finish posts layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react'
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core' 
+import React, { useEffect } from 'react'
+import {useDispatch} from 'react-redux'
+import {getPosts} from './actions/posts'
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core' 
 import memories from './images/memories.png'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
@@ -8,6 +10,10 @@ import useStyles from './styles'
 
 const App = () => {
     const classes = useStyles()
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getPosts())
+    }, [])
     return (
         <Container maxWidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
--- server/index.js
+++ server/index.js
@@ -7,12 +7,12 @@ import postRouter from './routers/posts.js'
 
 const app = express()
 
-app.use('/posts', postRouter)
 
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 
 app.use(cors())
+app.use('/posts', postRouter)
 
 const CONNECTION_URL = 'mongodb+srv://pavel:pavel123@cluster0.3gw8z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
