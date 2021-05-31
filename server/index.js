2ed82c79ec86f8490628318a9c2fe920f3db1777
Pavel Krokhmal <pavel.krokhmal.mn@outlook.com>
2021-05-31 13:56:19 +0300

    added actions constants dotenv

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 import postRouter from './routers/posts.js'
 
 const app = express()
-
+dotenv.config()
 
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
@@ -14,12 +15,10 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors())
 app.use('/posts', postRouter)
 
-const CONNECTION_URL = 'mongodb+srv://pavel:pavel123@cluster0.3gw8z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
 const PORT = process.env.PORT || 5000
 
 mongoose.set('useFindAndModify', false);
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
         app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
     })
